e94f2e657c8105ba406aa521b551ad1f154aa45a
flavioccf <flavioccf@hotmail.com>
2024-06-21 21:01:04 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
